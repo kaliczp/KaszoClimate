32769231cd9e9ca807fe209bf6bae7810c0be4be
Péter Kalicz <kaliczp@gmail.com>
2018-10-06 12:51:42 +0200

    xts gyártás és végignézés

--- Readme.R
+++ Readme.R
@@ -14,3 +14,15 @@ my_data <- read_xls(ttfile[1],range=cell_cols("A:T"))
 my_data.df <- as.data.frame(my_data)
 
 plot(my_data.df[,1],my_data.df[,2],type="l")
+
+library(xts)
+
+my_data <- read_xls(ttfile[2],range=cell_cols("A:T"))
+my_data.df <- as.data.frame(my_data)
+my_data.xts <- xts(my_data.df[,-1],my_data.df[,1])
+
+## Végignézés
+ttnames <- names(my_data.xts)
+tti <- 1
+
+plot(my_data.xts[,tti], main=ttnames[tti]);tti <- tti+1

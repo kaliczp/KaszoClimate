3dead3109aaa436fbec8f5a54ee261babeafde7a
Péter Kalicz <kaliczp@gmail.com>
2018-10-07 08:22:13 +0200

    Clean script
    Az előzetes próbálkozás törölve.

--- Readme.R
+++ Readme.R
@@ -1,31 +1,12 @@
+## Telepítendő csomagok: xts readxl
+
 ## A meteorológiai állomás 10 perces elemi adatai, illetve az azokból képzett
 ## statisztikák innen tölthetők le:
 ## http://met.boreas.hu/erti/
 ## wgettel leszedve az Excels könyvtárba letolt.txt letolt2.txt
 
-## Loading
-library("readxl")
 # xls files
 ttfile <- dir("Excels", patt="xls", full.names=TRUE)
-
-## my_data <- read_excel(ttfile[1],range=cell_cols("A:T"))
-my_data <- read_xls(ttfile[1],range=cell_cols("A:T"))
-
-my_data.df <- as.data.frame(my_data)
-
-plot(my_data.df[,1],my_data.df[,2],type="l")
-
-library(xts)
-
-my_data <- read_xls(ttfile[2],range=cell_cols("A:T"))
-my_data.df <- as.data.frame(my_data)
-my_data.xts <- xts(my_data.df[,-1],my_data.df[,1])
-
-## Végignézés
-ttnames <- names(my_data.xts)
-tti <- 1
-
-plot(my_data.xts[,tti], main=ttnames[tti]);tti <- tti+1
 
 beolv_boreasxls <- function(file) {
     require(xts)
@@ -34,5 +15,8 @@ beolv_boreasxls <- function(file) {
     df_data <- as.data.frame(tbl_data)
     xts(df_data[,-1],df_data[,1])
 }
+
+ttbeolv2 <- beolv_boreasxls(ttfile[2])
+ttnames <- names(ttbeolv2)
 
 plot(ttbeolv2[,tti], main=ttnames[tti]);tti <- tti+1
